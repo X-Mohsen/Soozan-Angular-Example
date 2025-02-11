c7528cd95e790de99e147b0ce69ed2c23dbffc29
Nazgol Yazdanpanah <nazgol.yazdanpanah@gmail.com>
2025-02-12 01:04:50 +0330

    added profile-page component

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -1,2 +1,3 @@
 export * from './dashboard-page/dashboard-page.component';
 export * from './orders-page/orders-page.component';
+export * from './profile-page/profile-page.component';
---
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'profile-page',
+  imports: [],
+  templateUrl: './profile-page.component.html',
+  styleUrl: './profile-page.component.scss',
+})
+export class ProfilePageComponent {}
