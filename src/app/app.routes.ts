1d1e7f2c1d968a17ef1bb69254b9982796905af2
Nazgol Yazdanpanah <nazgol.yazdanpanah@gmail.com>
2025-02-11 16:36:40 +0330

    added panel-layout to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PanelLayoutComponent } from './layouts';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PanelLayoutComponent,
+  },
+];
