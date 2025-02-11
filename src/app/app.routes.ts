228090b2e454f89dd473a9ba8f01c893dca55354
Nazgol Yazdanpanah <nazgol.yazdanpanah@gmail.com>
2025-02-12 01:01:51 +0330

    updated routes & fixed dashboard & navbar styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
 import { PanelLayoutComponent } from './layouts';
+import { DashboardPageComponent } from './pages';
 
 export const routes: Routes = [
   {
     path: '',
     component: PanelLayoutComponent,
+
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+
+      {
+        path: 'dashboard',
+        component: DashboardPageComponent,
+      },
+    ],
   },
 ];
--- src/app/layouts/panel-layout/panel-layout.component.ts
+++ src/app/layouts/panel-layout/panel-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared';
 import { PanelSidebarComponent } from '../../features';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-panel-layout',
-  imports: [NavbarComponent, PanelSidebarComponent],
+  imports: [NavbarComponent, RouterOutlet, PanelSidebarComponent],
   templateUrl: './panel-layout.component.html',
   styleUrl: './panel-layout.component.scss',
 })
