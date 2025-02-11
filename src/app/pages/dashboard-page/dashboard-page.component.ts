c5f5ae7e82c2265db85339a44c7606945a162f69
Nazgol Yazdanpanah <nazgol.yazdanpanah@gmail.com>
2025-02-12 00:04:52 +0330

    added dashboard-page & fixed icon color in navbar

---
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'dashboard-page',
+  templateUrl: './dashboard-page.component.html',
+  styleUrl: './dashboard-page.component.scss',
+})
+export class DashboardPageComponent {}
