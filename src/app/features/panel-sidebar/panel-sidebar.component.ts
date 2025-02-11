8ab9012dfe88c84aaec11c89bef7dc7a99787556
Nazgol Yazdanpanah <nazgol.yazdanpanah@gmail.com>
2025-02-12 01:16:15 +0330

    added routerLinkActive to panel-sidebar

--- src/app/features/panel-sidebar/panel-sidebar.component.ts
+++ src/app/features/panel-sidebar/panel-sidebar.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   selector: 'panel-sidebar',
   templateUrl: './panel-sidebar.component.html',
   styleUrl: './panel-sidebar.component.scss',
